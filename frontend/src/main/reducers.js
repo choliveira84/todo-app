3e4085ba0f416c00267c26af64663f882b281429
cho <cho@cesar.org.br>
2019-01-16 13:16:42 -0300

    redux

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,8 +1,12 @@
-import {combineReducers} from 'redux'
+import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
     todo: () => ({
-
+        description: 'Ler livro',
+        list: [
+            { id: 1, description: 'Pagar a fatura do cartão.', done: false },
+            { id: 2, description: 'Agendar médico.', done: true }
+        ]
     })
 })
 
